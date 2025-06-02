4850f4aee4d87597def19d7a4b4951f410d41f77
diffrenttechnology <lal@diffrentagency.com>
2025-06-02 11:15:32 +0200

    revert

--- src/workflows/wrapper.js
+++ src/workflows/wrapper.js
@@ -1,28 +1,3 @@
-// MessageChannel polyfill for Cloudflare Workers
-if (typeof globalThis.MessageChannel === 'undefined') {
-  globalThis.MessageChannel = class MessageChannel {
-    constructor() {
-      const channel = new BroadcastChannel('message-channel-polyfill');
-      this.port1 = {
-        postMessage: (data) => channel.postMessage(data),
-        close: () => channel.close()
-      };
-      this.port2 = {
-        postMessage: (data) => channel.postMessage(data),
-        close: () => channel.close()
-      };
-      channel.onmessage = (event) => {
-        if (this.port1.onmessage) {
-          this.port1.onmessage(event);
-        }
-        if (this.port2.onmessage) {
-          this.port2.onmessage(event);
-        }
-      };
-    }
-  };
-}
-
 // This is a wrapper file for exporting both the Astro application as well as
 // the CustomerWorkflow class. This is necessary because Astro does not allow
 // us to manually export non-Astro stuff as part of the bundle file.
